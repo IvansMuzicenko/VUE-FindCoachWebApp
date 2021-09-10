9a7dd98fc82b73700f59b609f72c584250a98594
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-09-10 09:53:27 +0300

    Attaching token to outgoing requests

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,5 +1,8 @@
 export default {
   useeId(state) {
     return state.userId;
+  },
+  token(state) {
+    return state.token;
   }
 };
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -28,8 +28,10 @@ export default {
   },
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://vue-find-coach-31575-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`
+      `https://vue-find-coach-31575-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json?auth=` +
+        token
     );
     const responseData = await response.json();
 
